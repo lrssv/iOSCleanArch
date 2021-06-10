1c91ccd853b33b328c15171ed31d5fd07253465a
Larissa Silva <larissa.silva@gerencianet.com.br>
2021-06-10 12:21:09 -0300

    ensure RemoteAddAccount calls httpClient with data

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Domain
 
 class RemoteAddAccount {
     private let url: URL
@@ -9,23 +10,33 @@ class RemoteAddAccount {
         self.httpClient = httpClient
     }
     
-    func add(){
-        httpClient.post(url: url)
+    func add(addAccountModel: AddAccountModel){
+        let data = try? JSONEncoder().encode(addAccountModel)
+        httpClient.post(to: url, with: data)
     }
 }
 
 protocol HttpPostClient {
-    func post(url: URL)
+    func post(to url: URL, with data: Data?)
 }
 
 class RemoteAddAccountTests: XCTestCase {
-
     func test_add_should_call_httpClient_with_correct_url(){
         let url = URL(string: "http://any-url.com")!
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy) //system under test
-        sut.add()
+        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
+        sut.add(addAccountModel: addAccountModel)
         XCTAssertEqual(httpClientSpy.url, url)
+    }
+    
+    func test_add_should_call_httpClient_with_correct_data(){
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy) //system under test
+        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
+        sut.add(addAccountModel: addAccountModel)
+        let data = try? JSONEncoder().encode(addAccountModel)
+        XCTAssertEqual(httpClientSpy.data, data)
     }
 }
 
@@ -34,9 +45,11 @@ extension RemoteAddAccountTests {
     //Versao mockada do protocolo HttpClient
     class HttpClientSpy: HttpPostClient {
         var url: URL?
+        var data: Data?
         
-        func post(url: URL){
+        func post(to url: URL, with data: Data?){
             self.url = url
+            self.data = data
         }
     }
 }
