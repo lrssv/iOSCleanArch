0831810e94271787ac55c356f96a5ee335b7dde1
Larissa Silva <larissa.silva@gerencianet.com.br>
2021-06-11 12:16:40 -0300

    ensure RemoteAddCcount completes with error when HttpClient complete with error

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -5,16 +5,27 @@ import Data
 class RemoteAddAccountTests: XCTestCase {
     func test_add_should_call_httpClient_with_correct_url(){
         let url = URL(string: "http://any-url.com")!
-        let (sut, httpClientSpy) = makeSut(url: url) //system under test
-        sut.add(addAccountModel: makeAddAccountModel())
-        XCTAssertEqual(httpClientSpy.url, url)
+        let (sut, httpClientSpy) = makeSut(url: url)
+        sut.add(addAccountModel: makeAddAccountModel()) { _ in }
+        XCTAssertEqual(httpClientSpy.urls, [url])
     }
     
     func test_add_should_call_httpClient_with_correct_data(){
-        let (sut, httpClientSpy) = makeSut() //system under test
+        let (sut, httpClientSpy) = makeSut()
         let addAccountModel = makeAddAccountModel()
-        sut.add(addAccountModel: addAccountModel)
+        sut.add(addAccountModel: addAccountModel) { _ in }
         XCTAssertEqual(httpClientSpy.data, addAccountModel.toData())
+    }
+    
+    func test_add_should_complete_with_error_if_client_fails(){
+        let (sut, httpClientSpy) = makeSut()
+        let exp = expectation(description: "waiting")
+        sut.add(addAccountModel: makeAddAccountModel()) { error in
+            XCTAssertEqual(error, .unexpected)
+            exp.fulfill()
+        }
+        httpClientSpy.completeWithError(.noConnectivity)
+        wait(for: [exp], timeout: 1)
     }
 }
 
@@ -22,7 +33,7 @@ class RemoteAddAccountTests: XCTestCase {
 extension RemoteAddAccountTests {
     func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteAddAccount, httpClientSpy: HttpClientSpy) {
         let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy) //system under test
         return (sut, httpClientSpy)
     }
     
@@ -32,13 +43,20 @@ extension RemoteAddAccountTests {
     
     //Versao mockada do protocolo HttpClient
     class HttpClientSpy: HttpPostClient {
-        var url: URL?
+        var urls = [URL]()
         var data: Data?
+        var completion: ((HttpError) -> Void)?
         
-        func post(to url: URL, with data: Data?){
-            self.url = url
+        func post(to url: URL, with data: Data?, completion: @escaping (HttpError) -> Void){
+            self.urls.append(url)
             self.data = data
+            self.completion = completion
+        }
+        
+        func completeWithError(_ error: HttpError){
+            completion?(error)
         }
     }
+    
 }
 
