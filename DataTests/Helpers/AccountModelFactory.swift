2d93137baaa9eb9fd3f19098ec999fb329b709a2
Larissa Silva <larissa.silva@gerencianet.com.br>
2021-06-14 10:33:03 -0300

    section 2: "Domain e Data Layer" was finished

---
+++ DataTests/Helpers/AccountModelFactory.swift
@@ -0,0 +1,7 @@
+import Foundation
+import Domain
+
+
+func makeAccountModel() -> AccountModel {
+    return AccountModel(id: "any_id", name: "any_name", email: "any_email@mail.com", password: "any_password")
+}  
