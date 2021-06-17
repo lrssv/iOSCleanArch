91b824e60df126c588be8b6bbd3a70ac93d7874a
Larissa Silva <larissa.silva@gerencianet.com.br>
2021-06-17 15:24:06 -0300

    ensure SignUpButton was clicked

--- UI/SignUpViewController.swift
+++ UI/SignUpViewController.swift
@@ -1,11 +1,39 @@
 import Foundation
-
+import Presentation
 import UIKit
 
 final class SignUpViewController: UIViewController {
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var signUp: ((SignUpViewModel) -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        saveButton?.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func saveButtonTapped(){
+        signUp?(SignUpViewModel(name: nil, email: nil, password: nil, passwordConfirmation: nil))
+    }
+}
+
+extension SignUpViewController: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        if viewModel.isLoading {
+            loadingIndicator?.startAnimating()
+        } else {
+            loadingIndicator?.stopAnimating()
+        }
+    }
+}
+
+extension SignUpViewController: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        
     }
 }
--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -1,13 +1,52 @@
 import XCTest
 import UIKit
+import Presentation
 @testable import UI
 
 class SignUpViewControllerTests: XCTestCase {
 
     func test_loading_is_hidden_on_start(){
+        XCTAssertEqual(makeSut().loadingIndicator?.isAnimating, false)
+    }
+    
+    func test_sut_implements_loadingView(){
+        XCTAssertNotNil(makeSut() as LoadingView)
+    }
+    
+    func test_sut_implements_alertView(){
+        XCTAssertNotNil(makeSut() as AlertView)
+    }
+    
+    func test_saveButton_calls_signUp_on_tap(){
+        var callsCount = 0
+        let sut = makeSut() { _ in
+            callsCount += 1
+        }
+        sut.saveButton?.simulateTap()
+        XCTAssertEqual(callsCount, 1)
+    }
+}
+
+extension SignUpViewControllerTests{
+    func makeSut(signUpSpy: ((SignUpViewModel) -> Void)? = nil) -> SignUpViewController {
         let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
         let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        sut.signUp = signUpSpy
         sut.loadViewIfNeeded()
-        XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
+        return sut
+    }
+}
+
+extension UIControl{
+    func simulate(event: UIControl.Event){
+        allTargets.forEach{ target in
+            actions(forTarget: target, forControlEvent: event)?.forEach{ action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap(){
+        simulate(event: .touchUpInside)
     }
 }
